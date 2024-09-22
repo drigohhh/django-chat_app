730093f552a86d3626aa326b8434046537172933
rodri <25470856+drigohhh@users.noreply.github.com>
2024-09-22 18:14:52 -0300

    Logging config for custom console logs

--- core/project/settings/__init__.py
+++ core/project/settings/__init__.py
@@ -17,5 +17,10 @@ if not os.path.isabs(LOCAL_SETTINGS_PATH):
     LOCAL_SETTINGS_PATH = str(BASE_DIR / LOCAL_SETTINGS_PATH)
 
 include(
-    "base.py", "custom.py", optional(LOCAL_SETTINGS_PATH), "envvars.py", "docker.py"
+    "base.py",
+    "logging.py",
+    "custom.py",
+    optional(LOCAL_SETTINGS_PATH),
+    "envvars.py",
+    "docker.py",
 )
--- core/project/settings/templates/settings.dev.py
+++ core/project/settings/templates/settings.dev.py
@@ -4,3 +4,12 @@
 
 DEBUG = True
 SECRET_KEY = ""
+
+LOGGING["formatters"]["colored"] = {  # type: ignore  # noqa: F821
+    "()": "colorlog.ColoredFormatter",
+    "format": "%(log_color)s%(asctime)s %(levelname)s %(name)s %(bold_white)s%(message)s",
+}
+
+LOGGING["loggers"]["core"]["level"] = "DEBUG"  # type: ignore  # noqa: F821
+LOGGING["handlers"]["console"]["level"] = "DEBUG"  # type: ignore  # noqa: F821
+LOGGING["handlers"]["console"]["formatter"] = "colored"  # type: ignore  # noqa: F821
